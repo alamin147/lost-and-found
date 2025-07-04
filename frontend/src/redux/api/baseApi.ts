cc9bc54ca576923f8253c9058197345aa56c619a
alamin147 <alamin.14780@gmail.com>
2025-07-04 22:01:42 +0600

    redux apis

---
+++ frontend/src/redux/api/baseApi.ts
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { getUserLocalStorage } from "../../auth/auth";
+
+export const baseApi = createApi({
+  reducerPath: "baseApi",
+  baseQuery: fetchBaseQuery({
+    // baseUrl: "http://localhost:5000/api",
+    baseUrl: `${import.meta.env.VITE_SERVER_URL}/api`,
+
+    credentials: "include",
+    prepareHeaders: (headers, {  }) => {
+      const token = getUserLocalStorage();
+
+      if (token) {
+        headers.set("authorization", `${token}`);
+      }
+      return headers;
+    },
+  }),
+  tagTypes: ["mylostItems", "myFoundItems", "users", "adminData"],
+
+  endpoints: () => ({}),
+});
